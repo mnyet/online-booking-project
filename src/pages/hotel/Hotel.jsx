d41dacc94f7a843432fff6f434b76ed10435af99
mnyet <janbercalamba10@gmail.com>
2022-09-14 12:07:19 +0800

    Sixth Commit | Finished Frontend, Proceeds to Backend.

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,119 @@
-import React from 'react'
+import "./hotel.css"
+import Navbar from "../../components/navbar/Navbar"
+import MainHeader from "../../components/header/MainHeader"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowLeft, faArrowRight, faLocationDot, faXmark } from "@fortawesome/free-solid-svg-icons"
+import MailList from "../../components/mailList/MailList"
+import Footer from "../../components/footer/Footer"
+import { useState } from "react"
 
 const Hotel = () => {
+
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const photos = [
+    {
+      src: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+    },
+    {
+      src: "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      src: "https://images.unsplash.com/photo-1535827841776-24afc1e255ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+    },
+    {
+      src: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      src: "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+      src: "https://images.unsplash.com/photo-1535827841776-24afc1e255ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+    }
+  ];
+
+  const handleOpen = (i) => {
+    setSlideNumber(i);
+    setOpen(true);
+  };
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if(direction==="l"){
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber-1;
+    } else{
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber+1;
+    }
+
+    setSlideNumber(newSlideNumber);
+  }
+
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <MainHeader type="list" />
+      <div className="hotelContainer">
+        {open && <div className="imgSlider">
+          <FontAwesomeIcon icon={faXmark} className="close" onClick={()=>setOpen(false)} />
+          <FontAwesomeIcon icon={faArrowLeft} className="arrow" onClick={()=>handleMove("l")} />
+          <div className="sliderWrapper">
+            <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+          </div>          
+          <FontAwesomeIcon icon={faArrowRight} className="arrow" onClick={()=>handleMove("r")} />
+        </div>}
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now</button>
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Adress here</span>
+          </div>
+          <div className="hotelDistance">
+            Distance from center here
+          </div>
+          <div className="hotelPriceHighlight">
+            Highlights here
+          </div>
+          <div className="hotelImages">
+            {photos.map((photo, i)=>(
+                <div className="hotelImgWrapper">
+                  <img 
+                    onClick={()=>handleOpen(i)} 
+                    src={photo.src} alt=""
+                    className="hotelImg"
+                  />
+                </div>
+              ))}
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Hotel Title</h1>
+              <p className="hotelDesc">
+                Hotel DescriptionHotel DescriptionHotel DescriptionHotel DescriptionHotel Description
+                Hotel DescriptionHotel DescriptionHotel DescriptionHotel DescriptionHotel Description
+                Hotel DescriptionHotel DescriptionHotel DescriptionHotel DescriptionHotel Description
+                Hotel DescriptionHotel DescriptionHotel DescriptionHotel DescriptionHotel Description
+                Hotel DescriptionHotel DescriptionHotel DescriptionHotel DescriptionHotel Description
+                </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for 9-night stay!</h1>
+              <span>
+                Located in chuchuchuchuchuc
+              </span>
+              <h2>
+                <b>Price</b> (2 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+        <MailList />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
